49400b0adf2c40201d4892a9395034eb34dcefbb
leopoldkwok <leopoldkwok@gmail.com>
2021-07-07 00:17:53 +0100

    Making our NotesList/Note Dynamic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note!',
+      date: '15/04/2021',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my second note!',
+      date: '21/04/2021',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my third note!',
+      date: '28/04/2021',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my new note!',
+      date: '30/04/2021',
+    },
+  ]);
+
   return (
     <div className='container'>
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
